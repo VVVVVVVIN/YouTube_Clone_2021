1d9a478c2921b1c707518e4a6d5a0cbc897a079f
VVIN <binibin11@naver.com>
2021-03-09 21:08:51 +0900

    #3.6(1) - 210309

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,33 +1,36 @@
-import routes from "../routes"
+import routes from "../routes";
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  try{
+  try {
     const videos = await Video.find({});
     res.render("Home", { pageTitle: "Home", videos });
-  } catch(error){
-      console.log(error)
-      res.render("Home", { pageTitle: "Home", videos: [] });
+  } catch (error) {
+    console.log(error);
+    res.render("Home", { pageTitle: "Home", videos: [] });
   }
 };
 export const search = (req, res) => {
-    const{query: { term: searchingBy } } = req;
-    res.render("Search", { pageTitle: "Search", searchingBy, videos});
-    }
-export const getUpload = (req, res) => 
-    res.render("Upload", { pageTitle: "Upload"});
+  const {
+    query: { term: searchingBy },
+  } = req;
+  res.render("Search", { pageTitle: "Search", searchingBy, videos });
+};
+export const getUpload = (req, res) =>
+  res.render("Upload", { pageTitle: "Upload" });
 
 export const postUpload = (req, res) => {
-    const {
-        body: { file, title, description }
-    } = req;
-    // To Do: Upload and save video
-    res.redirect(routes.videoDetail(9285));
+  const {
+    body: { file, title, description },
+  } = req;
+  console.dir(file, title, description);
+  // To Do: Upload and save video
+  res.redirect(routes.videoDetail(9285));
 };
 
-export const videoDetail = (req, res) => 
-    res.render("VideoDetail", { pageTitle: "Video Detail"});
+export const videoDetail = (req, res) =>
+  res.render("VideoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) =>
-    res.render("EditVideo", { pageTitle: "Edit Video"});
-export const deleteVideo = (req, res) => 
-    res.render("DeleteVideo", { pageTitle: "Delete Video"});
+  res.render("EditVideo", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) =>
+  res.render("DeleteVideo", { pageTitle: "Delete Video" });
