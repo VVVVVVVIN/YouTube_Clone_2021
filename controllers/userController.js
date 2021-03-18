069ecf3b78b0fe9152b1bccb5ffc55e9a77e2212
VVIN <binibin11@naver.com>
2021-03-18 20:44:56 +0900

    #6.3 - 210318

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+import passport from "passport"
 import routes from "../routes"
 import User from "../models/User";
 
@@ -5,7 +6,7 @@ export const getJoin = (req, res) => {
  res.render("Join", { pageTitle: "Join"});
 };
 
-export const postJoin = async(req, res) => {
+export const postJoin = async(req, res, next) => {
   const {
     body: { name, email, password, password2 }
   } = req;
@@ -19,19 +20,20 @@ export const postJoin = async(req, res) => {
         email
       });
       await User.register(user, password);
+      next();
     } catch(error){
       console.log(error);
-    }
-      // To Do: Log user in
       res.redirect(routes.home);
+    }
   }
 };
 
 export const getLogin = (req, res) => 
   res.render("Login", { pageTitle: "Login"});
-export const postLogin = (req, res) => {
-  res.redirect(routes.home);
-};
+export const postLogin = passport.authenticate('local', {
+  failureRedirect: routes.login,
+  successRedirect: routes.home
+})
 
 export const logout = (req, res) => {
   // To Do: Process Log Out
