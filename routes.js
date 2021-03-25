02938fe5bb0a5ab3096628743c89ff73b282a05c
VVIN <binibin11@naver.com>
2021-03-25 16:14:18 +0900

    #10.0 - 210325

--- routes.js
+++ routes.js
@@ -22,10 +22,15 @@ const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
-//Github
+// Github
 
 const GITHUB = "/auth/github";
 const GITHUB_CALLBACK = "/auth/github/callback";
+
+// API
+
+const API = "/api";
+const REGISTER_VIEW = "/:id/view";
 
 const routes = {
   home: HOME,
@@ -68,7 +73,9 @@ const routes = {
   },
   gitHub: GITHUB,
   githubCallback: GITHUB_CALLBACK,
-  me: ME
+  me: ME,
+  api: API,
+  registerView: REGISTER_VIEW
 };
 
 
